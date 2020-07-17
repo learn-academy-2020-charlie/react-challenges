2727590805fd6da8cea0ff9192b0dcb4c2625f84
selamhabteab <selamhabteab@gmail.com>
2020-07-17 12:59:19 -0700

    completed user stories, still working on bad robot component

---
+++ robots-seltrip/src/components/Mathewrobot.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Mathewrobot extends Component {
+
+    render(){
+        return (
+            <>
+                <label>Matthew McConaughey!!</label>
+                <p>
+                I hear you saying, "{this.props.text}." Is that alright, alright, alright?
+                </p>
+            </>
+        )
+    }
+}
+
+export default Mathewrobot
