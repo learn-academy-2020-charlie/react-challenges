0b0331c2fd455bff594013bc7efe3a17191b6f09
ryankoch13 <ryan.koch13@gmail.com>
2020-07-17 11:59:17 -0700

    first challenges started

---
+++ mr-robot-form/src/components/Robot3.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Robot3 extends Component {
+    render(){
+        return(
+            <React.Fragment>
+                <p> { this.props.userInput } </p>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Robot3
